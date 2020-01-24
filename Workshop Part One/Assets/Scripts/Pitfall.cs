1e36523383324cb995adcd19559bf6ebfc2658a0
Kevin Losada <kevin.losada21@gmail.com>
2020-01-24 00:11:24 -0500

    Part one complete

---
+++ Workshop Part One/Assets/Scripts/Pitfall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pitfall : MonoBehaviour
+{
+    public GameObject spawn;
+    public GameObject powerUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        powerUp.transform.gameObject.SetActive(true);
+        other.transform.position = spawn.transform.position;
+        other.transform.rotation = spawn.transform.rotation;
+    }
+}
---
+++ Workshop Part One/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    SimpleCharacterControl PlayScript;
+    GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlayScript = Player.GetComponent<SimpleCharacterControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag.Equals("PowerUp"))
+        {
+            other.transform.gameObject.SetActive(false);
+            PlayScript.canJump = true;
+        }
+    }
+}
